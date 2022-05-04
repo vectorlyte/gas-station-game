3597c7294220feb3c7f56c5a505cdbf5e370d3ea
Armand I <vectorlite@gmail.com>
2022-05-04 18:17:19 -0400

    added car spawning and despawning

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Intro
 
 This is gas station game, where you must fuel cars to keep your business going!
+
+## plans
+
+Player will have to fuel cars to make money to buy more fuel and weapons to fight off enemies.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const gameWindow = document.getElementById("gameWindow");
+let root = document.documentElement;
 
-function SpawnCar() {
+function spawnCar() {
     function randomRGB(n = 2){
         if(n < 0){
             return []
@@ -10,11 +11,33 @@ function SpawnCar() {
             return randomColor;
         }
     }
-    let car = `<div id="car" class="prop" style="background-color: rgb(${randomRGB()})"></div>`
+    let color = `rgb(${randomRGB()})`;
+    let car = `
+        <div id="car" class="prop"  style="background-color: ${color}" 
+        onclick="fuel()"
+            >
+            <div class="body" style="background-color: ${color}"></div>
+            <div class="front-wheels"></div>
+            <div class="rear-wheels"></div>
+            </div>`
 
     gameWindow.innerHTML += car;
 }
 
-SpawnCar();
+spawnCar();
 
 
+// make cars clickable, if clicked, fuel car.
+
+
+function fuel() {
+    root.style.setProperty("--animation-name", "driveAway");
+    const driveEnd = document.querySelector('#car');
+    driveEnd.addEventListener("animationend", () => {
+        gameWindow.removeChild(document.getElementById("car"));
+        root.style.setProperty("--animation-name", "drive");
+        spawnCar();
+    })
+}
+
+// const carX = document.getElementById("car").getBoundingClientRect();
