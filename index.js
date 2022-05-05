341b2641d71167c1daa56a938f1dbb219d6114af
Armand I <vectorlite@gmail.com>
2022-05-04 22:54:11 -0400

    added score system
    implemented rules for being able to click on car to fuel and score system

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const gameWindow = document.getElementById("gameWindow");
 let root = document.documentElement;
+let atStation = false;
+let score = 0;
+
+
 
 function spawnCar() {
+    
+    //<====> Generates a random RGB color <====>
     function randomRGB(n = 2){
         if(n < 0){
             return []
@@ -12,6 +18,8 @@ function spawnCar() {
         }
     }
     let color = `rgb(${randomRGB()})`;
+
+    // <====> Creates the car Object(div) <====>
     let car = `
         <div id="car" class="prop"  style="background-color: ${color}" 
         onclick="fuel()"
@@ -21,23 +29,40 @@ function spawnCar() {
             <div class="rear-wheels"></div>
             </div>`
 
+    //applies car to the DOM
     gameWindow.innerHTML += car;
+
+    //<====> Checks car has arrived at station <====>
+    const Arrived = document.querySelector('#car');
+    Arrived.addEventListener("animationend", () => {
+        atStation = true;
+        console.log(atStation);
+    })
 }
 
+//Spawns car
 spawnCar();
 
 
-// make cars clickable, if clicked, fuel car.
-
-
 function fuel() {
+    if(atStation){
     root.style.setProperty("--animation-name", "driveAway");
+    score += Math.floor(Math.random()*15 + 5);
+    updateScore();
     const driveEnd = document.querySelector('#car');
     driveEnd.addEventListener("animationend", () => {
         gameWindow.removeChild(document.getElementById("car"));
         root.style.setProperty("--animation-name", "drive");
         spawnCar();
+        atStation = false;
     })
 }
+}
+
+function updateScore() {
+    const scoreEl = document.getElementById("score");
+    scoreEl.innerText = `$:${score}`;
+}
+
 
 // const carX = document.getElementById("car").getBoundingClientRect();
